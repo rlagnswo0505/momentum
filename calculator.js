50cf2ae9d70587be373edfe077bdf4ab9313c017
rlagnswo0505 <rlagnswo0505@naver.com>
2022-04-19 16:55:42 +0900

    create calculator

---
+++ calculator.js
@@ -0,0 +1,23 @@
+const calculator = {
+    add: function (a, b) {
+        console.log(a + b);
+    },
+    minus: function (a, b) {
+        console.log(a - b);
+    },
+    div: function (a, b) {
+        console.log(a / b);
+    },
+    multi: function (a, b) {
+        console.log(a * b);
+    },
+    powerof: function (a, b) {
+        console.log(a ** b);
+    },
+};
+
+calculator.add(5, 1);
+calculator.minus(5, 1);
+calculator.div(5, 1);
+calculator.multi(5, 1);
+calculator.powerof(5, 1);
