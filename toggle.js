0dd28b399af909d9c8ed54cdb6534ea6cd9de3b1
rlagnswo0505 <rlagnswo0505@naver.com>
2022-04-24 22:36:12 +0900

    update css_in

--- toggle.js
+++
@@ -1,6 +0,0 @@
-const h1 = document.querySelector("div.hello:firt-child h1");
-
-function handleTitleClick() {
-    h1.classList.toggle("clicked");
-}
-h1.addEventListener("click", handleTitleClick);
