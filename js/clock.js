d79ef7b55c1831df0c3f7229eee89ba64c59ed98
rlagnswo0505 <rlagnswo0505@naver.com>
2022-05-06 23:47:53 +0900

    create getClock

--- js/clock.js
+++ js/clock.js
@@ -1,7 +1,8 @@
 const clock = document.querySelector('h2#clock');
 
-function sayHello() {
-    console.log('hello');
+function getClock() {
+    const date = new Date();
+    clock.innerHTML = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
-
-setTimeout(sayHello, 2000);
+getClock();
+setInterval(getClock, 1000);
