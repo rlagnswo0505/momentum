a84204c5beb1f8a2982e6ff4dc78ded49b30add2
rlagnswo0505 <rlagnswo0505@naver.com>
2022-05-08 18:49:19 +0900

    update quotes

--- js/quotes.js
+++ js/quotes.js
@@ -43,3 +43,8 @@ const quotes = [
 
 const quote = document.querySelector('#quote span:first-child');
 const author = document.querySelector('#quote span:last-child');
+
+const todaysQuote = quotes[Math.ceil(Math.random() * (quotes.length - 1))];
+
+quote.innerHTML = todaysQuote.quote;
+author.innerHTML = todaysQuote.author;
