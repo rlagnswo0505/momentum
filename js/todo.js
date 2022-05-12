b233d9c8697a0f2c0afd2dd9faaae7351c951bea
rlagnswo0505 <rlagnswo0505@naver.com>
2022-05-12 21:18:04 +0900

    update todo

--- js/todo.js
+++ js/todo.js
@@ -2,12 +2,17 @@ const toDoForm = document.querySelector('#todo-form');
 const toDoInput = toDoForm.querySelector('input');
 const toDoList = document.querySelector('#todo-list');
 
+function deleteToDo(event) {
+    console.log(event.target.parentElement);
+}
+
 function paintToDo(newTodo) {
     const li = document.createElement('li');
     const span = document.createElement('span');
     span.innerHTML = newTodo;
     const button = document.createElement('button');
     button.innerHTML = '❌';
+    button.addEventListener('click', deleteToDo);
     li.appendChild(span);
     li.appendChild(button);
     toDoList.appendChild(li);
