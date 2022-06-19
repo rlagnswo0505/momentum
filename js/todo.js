aa05f56a2d1f6815492dafd4fbca5d8fa5bdaaaa
rlagnswo0505 <rlagnswo0505@naver.com>
2022-06-19 22:54:33 +0900

    todo를 객체로 만들고 id 추가

--- js/todo.js
+++ js/todo.js
@@ -17,8 +17,9 @@ function deleteToDo(event) {
 
 function paintToDo(newTodo) {
   const li = document.createElement('li');
+  li.id = newTodo.id;
   const span = document.createElement('span');
-  span.innerHTML = newTodo;
+  span.innerHTML = newTodo.text;
   const button = document.createElement('button');
   button.innerHTML = '❌';
   button.addEventListener('click', deleteToDo);
@@ -31,7 +32,11 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = '';
-  toDos.push(newTodo);
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newTodoObj);
   paintToDo(newTodo);
   saveToDos(toDos);
 }
